bb8260884f633c72ded863714b80751aaad3c404
RD <ripudaman1999.kaushik@gmail.com>
2019-03-18 16:28:36 +0530

    added loader for username

--- views/signup.js
+++ views/signup.js
@@ -5,8 +5,8 @@ function check(){
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.send(`usrname=${x}`);
 	xhttp.onreadystatechange=()=>{
+		document.getElementById('load').style.display="none";
 		if(xhttp.status === 200){
-			document.getElementById('load').style.display="none";
 			document.getElementById('usrname-tick').style.display="inline-block";
 			if(xhttp.responseText==='ok'){
 				document.getElementById('usrname-tick').src="tick.png";
