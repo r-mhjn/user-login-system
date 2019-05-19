ae543c141d56190d4f69d6b6f6e1d26ebe5b20b8
RD <ripudaman1999.kaushik@gmail.com>
2019-05-20 02:38:17 +0530

    added form validation

--- views/signup.js
+++ views/signup.js
@@ -1,24 +1,51 @@
-function check(){
-	var x=document.getElementById('usrname').value;
-	var xhttp=new XMLHttpRequest();
-	xhttp.open('POST','http://localhost:8080/ajaxcall',true);
+function check() {
+	var x = document.getElementById('usrname').value;
+	var xhttp = new XMLHttpRequest();
+	xhttp.open('POST', 'http://localhost:8080/ajaxcall', true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.send(`username=${x}`);
-	xhttp.onreadystatechange=()=>{
-		document.getElementById('load').style.display="none";
-		if(xhttp.status === 200){
+	xhttp.onreadystatechange = () => {
+		document.getElementById('load').style.display = "none";
+		if (xhttp.status === 200) {
 			console.log(xhttp.responseText);
-			document.getElementById('usrname-tick').style.display="inline-block";
-			if(xhttp.responseText==='ok'){
-				document.getElementById('usrname-tick').src="tick.png";
-				document.getElementById('usrname-tick').title="username available";
-			} else{
-				document.getElementById('usrname-tick').src="cross.png";
-				document.getElementById('usrname-tick').title="username taken";
+			document.getElementById('usrname-tick').style.display = "inline-block";
+			if (xhttp.responseText === 'ok') {
+				document.getElementById('usrname-tick').src = "tick.png";
+				document.getElementById('usrname-tick').title = "username available";
+			} else {
+				document.getElementById('usrname-tick').src = "cross.png";
+				document.getElementById('usrname-tick').title = "username taken";
 			}
 		}
 	}
-	document.getElementById('load').style.display="inline-block";
+	document.getElementById('load').style.display = "inline-block";
 }
 
-//TODO: validate form then submit;
+function validate() {
+	const form = document.forms['signup'];
+
+	const name = form['name'].value;
+	const username = form['username'].value;
+	const pass = form['password'].value;
+	const reppass = form['reppasswd'].value;
+
+	console.log(name, username, pass, reppass);
+	if (name.length < 5) {
+		alert('Name should not be less than 5 characters!');
+		return false;
+	}
+	if (username.length < 3) {
+		alert('Username should not be less than 3 characters!');
+		return false;
+	}
+	if (pass.length < 5) {
+		alert('Password should not be less than 5 characters!');
+		return false;
+	}
+	if (pass != reppass) {
+		alert('passwords do not match!');
+		return false;
+	}
+
+	return true;
+}
