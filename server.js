8e38eb7baab1a1d4952c08f118b0e6b7f2822f64
Rachit Mahajan <rachitmahajan9936@gmail.com>
2020-10-01 22:01:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,32 @@ const session = require('express-session');
 const passport = require('passport');
 const morgan = require('morgan');
 const flash = require('connect-flash-plus');
-
 var app = express();
+const helmet = require("helmet");
+app.use(helmet());
+
+
+//Swagger setup
+const swaggerOptions = {
+  explorer: true,
+  swaggerDefinition: {
+    info: {
+      title: "Login System API",
+      description: "Login System API Documentation",
+      version: "1.0.0",
+      servers: [`http://localhost:${port}`],
+    },
+  },
+
+  apis: [
+    "server.js",  
+  ],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs-mpm", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
 
 const port = process.env.PORT | 8080;
 
